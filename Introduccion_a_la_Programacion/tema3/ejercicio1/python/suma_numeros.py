8858195db2e9d5516a9cd17a9a36733e726370c3
biojuampa <104794253+biojuampa@users.noreply.github.com>
2022-10-03 11:04:29 -0300

    Ejercicios tema 3 - Intrducción a la Informática

---
+++ Introduccion_a_la_Programacion/tema3/ejercicio1/python/suma_numeros.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  suma_numeros.py
+#  
+#  Copyright 2022 Juan Pablo Camussi <jcamussi@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+def suma3 (a, b, c):
+		
+	return (a + b + c)	# Los paréntesis no son necesarios,
+						# pero mejoran la legibilidad del código.	
+	
+# Main
+resultado = suma3(1, 2, 3)
+print(resultado)
---
+++ Introduccion_a_la_Programacion/tema3/ejercicio2/java/Vehiculo.java
@@ -0,0 +1,44 @@
+/*
+ * Vehiculo.java
+ * 
+ * Copyright 2022 Juan Pablo Camussi <jcamussi@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class Vehiculo {
+	
+	public static void main (String[] args) {
+		
+		Coche miCoche = new Coche();
+		miCoche.agregarPuerta();
+		System.out.println("Nro de puertas del coche: "
+							+ miCoche.puertas);
+	
+	}
+
+}
+
+class Coche {
+	int puertas = 0;
+	
+	public void agregarPuerta () {
+		this.puertas++;		
+	}
+}
