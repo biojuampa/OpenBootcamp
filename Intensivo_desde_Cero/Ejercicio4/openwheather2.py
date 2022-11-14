b77245767415d22c32c52bb26d2e1f04f6e59b61
biojuampa <104794253+biojuampa@users.noreply.github.com>
2022-11-14 00:52:37 -0300

    Ejercicio 4 (mejoras)
    Intensivo desde Cero

--- Intensivo_desde_Cero/Ejercicio4/openwheather2.py
+++ Intensivo_desde_Cero/Ejercicio4/openwheather2.py
@@ -21,13 +21,23 @@ URL = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{coutry_co
 
 with urllib.request.urlopen(URL) as data:
     info = json.loads(data.read())
+    #print(info)
 
 main = info['main']
+temp = round(main['temp'] - 273.15, 1)
 max_temp = round(main['temp_max'] - 273.15, 1)
 min_temp = round(main['temp_min'] - 273.15, 1)
+feels_like = round(main['feels_like'] - 273.15, 1)
+pressure = main['pressure']
+humidity = main['humidity']
 
 print()
-print(f'    Ciudad: {info["name"]}')
-print(f'    Temperatura máxima: {max_temp}°C')
-print(f'    Temperatura mínima: {min_temp}°C')
+print(f'  Ciudad: {info["name"]}')
+print()
+print(f'    Temperatura:          {temp}°C')
+print(f'    Temperatura máxima:   {max_temp}°C')
+print(f'    Temperatura mínima:   {min_temp}°C')
+print(f'    Sensación térmica:    {feels_like}°C')
+print(f'    Presión atmosférica:  {pressure} hPa')
+print(f'    Humedad relativa:     {humidity}%')
 print()
