874239dc648d3c4b9908221df63075677b991d99
biojuampa <104794253+biojuampa@users.noreply.github.com>
2022-11-14 00:33:06 -0300

    Ejercicio 4 - Intensivo desde Cero
    Intensivo desde Cero

---
+++ Intensivo_desde_Cero/Ejercicio4/Readme.md
@@ -0,0 +1,8 @@
+# Ejercicio 4 - Intensivo desde Cero
+
+__Enunciado:__ Utilizando la API de [https://openweathermap.org/](https://openweathermap.org/) y realizando una petición a [https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}](https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}), obtén la temperatura máxima y mínima, para la ciudad que proporcione el usuario.
+
+__Objetivo:__
+
+- Aprender a utilizar librerías externas (en este caso, requests)
+- Manipular el resultado de la petición (JSON)
---
+++ Intensivo_desde_Cero/Ejercicio4/openwheather2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+  openweather.py
+
+"""
+
+
+import urllib.request
+import json
+
+
+city_name = input('Dime una ciudad: ').strip().lower().replace(' ', '%20')
+coutry_code = input('Dime el código país (p.e. "es o esp" para España): ').strip().lower().replace(' ', '%20')
+
+
+LANG = 'es'
+API_KEY = 'c46e4eae44f551cc12b58f9d9423f232'
+URL = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{coutry_code}&appid={API_KEY}&lang={LANG}'
+
+
+with urllib.request.urlopen(URL) as data:
+    info = json.loads(data.read())
+
+main = info['main']
+max_temp = round(main['temp_max'] - 273.15, 1)
+min_temp = round(main['temp_min'] - 273.15, 1)
+
+print()
+print(f'    Ciudad: {info["name"]}')
+print(f'    Temperatura máxima: {max_temp}°C')
+print(f'    Temperatura mínima: {min_temp}°C')
+print()
