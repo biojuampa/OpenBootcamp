ed1236cd939b96ff2c29131f079fae0036d88eea
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-03-02 23:01:27 -0300

    Conceptos.
    Curso de java Básico

---
+++ Java_Basico/tema1/clases/Comentarios.java
@@ -0,0 +1,27 @@
+public class Comentarios {
+    public static void main (String[] args) {
+        // Este es un comentario simple
+
+        /*
+          Este es un comentario
+          multilínea.
+        */
+
+        /**
+          Este tipo de comentarios se
+          utiliza para documentar el 
+          código.
+
+          @param parámetro
+          @see verOtrasClases
+          @author autor<correo@demo.com>
+
+          Luego, con heramientas como apache label
+          podemos recopilar toda la documentación
+          incorporada en el código de esta manera.
+          
+        */
+
+        System.out.println("Leer este código para ver cómo se hacen los comentarios.");
+    }
+}
