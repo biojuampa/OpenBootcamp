aa269fcf80bbdf42ee527ab5454f8553e26cf58b
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-03-13 15:36:02 -0300

    Ejercicio 1 tema 7, 8 y 9
    Curso de java Básico

---
+++ Java_Basico/temas_7_8_9/ejercicio1/StringsArray.java
@@ -0,0 +1,11 @@
+public class StringsArray {
+    public static void main(String[] args) {
+        
+        String[] words = {"pablito", "clavó", "un", "clavito"};
+
+        for (String word: words)
+            System.out.println(word);
+
+    }
+}
+
