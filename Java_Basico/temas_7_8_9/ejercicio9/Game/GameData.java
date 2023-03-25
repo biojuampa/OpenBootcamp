fb1e2452c80f9188951a0c28b2b458734a43a111
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-03-25 10:49:00 -0300

    Ejercicio 9 - temas 7, 8 y 9
    Curso de java Básico

--- Java_Basico/temas_7_8_9/ejercicio9/Game/GameData.java
+++ Java_Basico/temas_7_8_9/ejercicio9/Game/GameData.java
@@ -1,7 +1,18 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
-import org.json.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.JSONException;
 
 
 public class GameData {
@@ -12,71 +23,202 @@ public class GameData {
     
     
     // Constructores
-    public void GameData() {}
+    public GameData() {}
 
-    public void GameData(String file) {
-
-        InputStream iStream = new InputStream(file);
-        readJSONFile(iStream);
-    
+    public GameData(String file) {
+        
+        readFromFile(file);
+        
     }
 
-    public void GameData(String url) {
-    
-        InputStream iStream = url.openStream();
-        readJSONFile(iStream);
+    public GameData(URL url) {
+
+        readFromURL(url);
 
     }
 
     
     // Métodos
     private void readJSONFile(InputStream iStream) {
-        
-        JSONTokener tokener = new JSONTokener(iStream);  
-        JSONObject jobj = new JSONObject(tokener);
-        
-        this.squadName = jobj.getString("squadName");
+        try {
 
-        JSONArray members = jobj.getJSONArray("members");
-        for (Object memberObj: members) {
-            JSONObject member = (JSONObject)memberObj;
-            String memberName = member.getString("name");
+            JSONTokener tokener = new JSONTokener(iStream);  
+            JSONObject jobj = new JSONObject(tokener);
             
-            ArrayList<String> powersList = new ArrayList<>();
-            JSONArray powers = member.getJSONArray("powers");
-            for (Object power: powers) {
-                powersList.add((String)power);
+            this.squadName = jobj.getString("squadName");
+    
+            JSONArray members = jobj.getJSONArray("members");
+            for (Object memberObj: members) {
+                JSONObject member = (JSONObject)memberObj;
+                String memberName = member.getString("name");
+                
+                ArrayList<String> powersList = new ArrayList<>();
+                JSONArray powers = member.getJSONArray("powers");
+                for (Object power: powers) {
+                    powersList.add((String)power);
+                }
+    
+                this.memberPowers.put(memberName, powersList);
+    
             }
 
-            this.memberPowers.put(memberName, powersList);
+        } catch (JSONException e) {
+
+            System.out.println("ERROR con el objeto JSON: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        }
+        
+    }
+
+    public void readFromFile(String file) {
+        try {
+        
+            InputStream iStream = new FileInputStream(file);
+            readJSONFile(iStream);
+    
+        } catch (FileNotFoundException e) {
+
+            System.out.printf("ERROR con el archivo %s: %s", file, e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+       
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        }
+    }
+
+    public void readFromURL(URL url) {
+        try {
+
+            InputStream iStream = url.openStream();
+            readJSONFile(iStream);
+
+        } catch (IOException e) {
+
+            System.out.println("ERROR de E/S: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+       
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+        
+        }
+
+    }
+
+    public void showData() {
+
+    }
+
+    public void saveToCSV() {
+        try {
+
+            PrintStream fileCSV = new PrintStream(squadName + ".csv");
+
+            fileCSV.println("Member,Powers");
+            for (String name: this.memberPowers.keySet()) {
+                String powers = "";
+                for (String power: this.memberPowers.get(name)) {
+                    powers += power + ",";
+                }
+
+                fileCSV.println(name + ",\"" + powers + "\"");
+            }
+
+            fileCSV.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.printf("ERROR con el archivo: %s", e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+       
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        }
+    }
+
+    public void saveToMD() {
+        try {
+
+            PrintStream fileMD = new PrintStream(squadName + ".md");
+            
+            fileMD.println("# Escuadrón: " + this.squadName);
+            fileMD.println();
+            fileMD.println("## Miembros");
+            fileMD.println();
+
+            for (String member: this.memberPowers.keySet()) {
+                fileMD.println("1. **" + member + "**");
+                for (String power: this.memberPowers.get(member))
+                    fileMD.println("    * _" + power + "_");
+            }
+            
+            fileMD.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.printf("ERROR con el archivo: %s", e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+       
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
 
         }
 
     }
 
-    public InputStream readFromFile(String file) {
-
-        InputStream iStream = new InputStream(file);
-        readJSONFile(iStream);
-    
-    }
-
-    public InputStream readFromURL(String url) {
-
-        InputStream iStream = url.openStream();
-        readJSONFile(iStream);
-
-    }
-
-    public void saveToCSV() {
-
-    }
-
-    public void saveToMD() {
-
-    }
-
     public void saveToJSON() {
+        try {
+
+            PrintStream fileJSON = new PrintStream(squadName + ".json");
+            
+            JSONObject jsonMain = new JSONObject();
+            //JSONObject member = new JSONObject();
+            JSONArray miembros = new JSONArray();
+
+            for (String name: this.memberPowers.keySet()) {
+                JSONObject member = new JSONObject();
+                member.put("name", name);
+                member.put("powers", this.memberPowers.get(name));
+                miembros.put(member);
+            }
+
+            jsonMain.put("squadName", this.squadName);
+            jsonMain.put("members", miembros);
+            fileJSON.println(jsonMain.toString(2));
+
+            fileJSON.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.printf("ERROR con el archivo: %s", e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+       
+        } catch (JSONException e) {
+
+            System.out.println("ERROR con el objeto JSON: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: " + e.getMessage());
+            System.out.println("ERROR: " + e.getClass());
+
+        }
 
     }
 }
--- Java_Basico/temas_7_8_9/ejercicio9/Game/GameMain.java
+++ Java_Basico/temas_7_8_9/ejercicio9/Game/GameMain.java
@@ -1,77 +1,24 @@
+import java.net.URL;
+import java.net.MalformedURLException;
+
+
 public class GameMain {
     public static void main(String[] args) {
    
         try {
 
             URL url = new URL("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
-            InputStream iStream = url.openStream();
+            GameData gd = new GameData(url);
 
+            gd.saveToCSV();
+            gd.saveToMD();
+            gd.saveToJSON();
 
-            PrintStream file = new PrintStream(squadName + ".md");
-            
-            file.println("# Escuadrón: " + squadName);
-            file.println();
-            file.println("## Miembros");
-            file.println();
-
-            for (String member: memberPowers.keySet()) {
-                file.println("1. **" + member + "**");
-                for (String power: memberPowers.get(member))
-                    file.println("    * _" + power + "_");
-            }
-            
-            file.close();
-
-            PrintStream fileCSV = new PrintStream(squadName + ".csv");
-
-            fileCSV.println("Member,Powers");
-            for (String name: memberPowers.keySet()) {
-                String powers = "";
-                for (String power: memberPowers.get(name)) {
-                    powers += power + ",";
-                }
-
-                fileCSV.println(name + ",\"" + powers + "\"");
-            }
-
-            fileCSV.close();
-
-            PrintStream fileJSON = new PrintStream(squadName + ".json");
-            
-            JSONObject jsonMain = new JSONObject();
-            //JSONObject member = new JSONObject();
-            JSONArray miembros = new JSONArray();
-
-            for (String name: memberPowers.keySet()) {
-                do { // uso el do-while con el único objetivo de crear un nuevo objeto JSONObject en cada iteración del foreach
-                    JSONObject member = new JSONObject();
-                    member.put("name", name);
-                    member.put("powers", memberPowers.get(name));
-                    miembros.put(member);
-                } while (false);
-            }
-
-            jsonMain.put("squadName", squadName);
-            jsonMain.put("members", miembros);
-            fileJSON.println(jsonMain.toString(2));
-
-            fileJSON.close();
-
-        } catch (FileNotFoundException e) {
-
-            System.out.println("ERROR con el archivo: " + e.getMessage());
-            System.out.println("ERROR: " + e.getClass());
-       
         } catch (MalformedURLException e) {
  
             System.out.println("ERROR en la URL: " + e.getMessage());
             System.out.println("ERROR: " + e.getClass());
        
-        } catch (IOException e) {
-
-            System.out.println("ERROR de E/S: " + e.getMessage());
-            System.out.println("ERROR: " + e.getClass());
-        
         } catch (Exception e) {
 
             System.out.println("ERROR: " + e.getMessage());
