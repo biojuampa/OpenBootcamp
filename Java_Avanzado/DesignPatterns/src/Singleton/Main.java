fa87797a37bf8802ed2576a2d5985e755bc22dc4
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-05-23 13:29:07 -0300

    Patrones de diseño
    Curso de Java Avanzado

---
+++ Java_Avanzado/DesignPatterns/src/Singleton/Main.java
@@ -0,0 +1,19 @@
+package Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        singleton.setContador(12);
+
+        System.out.println(singleton.getContador() + " " + singleton);
+        System.out.println(singleton2.getContador() + " " + singleton2);
+
+        singleton2.setContador(33);
+
+        System.out.println(singleton.getContador() + " " + singleton);
+        System.out.println(singleton2.getContador() + " " + singleton2);
+
+    }
+}
---
+++ Java_Avanzado/DesignPatterns/src/Singleton/Singleton.java
@@ -0,0 +1,27 @@
+package Singleton;
+
+public class Singleton {
+
+    // Atributos
+    private int contador;
+    private static Singleton singleton;
+
+    // Constructor
+    private Singleton() {}
+
+    // Métodos
+    public static Singleton getInstance() {
+        if (singleton == null)
+            singleton = new Singleton();
+
+        return singleton;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+}
