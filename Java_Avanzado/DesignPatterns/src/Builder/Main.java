f4d26e6537300d27e76d61267142b4f6afd2b186
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-05-27 00:31:13 -0300

    Builder pattern.
    Curso de Java Avanzado

--- Java_Avanzado/DesignPatterns/src/Builder/Main.java
+++ Java_Avanzado/DesignPatterns/src/Builder/Main.java
@@ -2,7 +2,14 @@ package Builder;
 
 public class Main {
     public static void main(String[] args) {
-        Coche coche1 = new CocheBuilder("Ford")
+//        Coche coche = new Coche("Ford");
+
+//        coche.setMotor("Nafta");
+//        coche.setModelo("Focus");
+//        coche.setPotencia(170);
+//        coche.setPuertas(5);
+
+        Coche coche = new CocheBuilder("Ford")
                 .setMotor("Nafta")
                 .setModelo("Focus")
                 .setPotencia(170)
@@ -10,23 +17,10 @@ public class Main {
                 .build()
                 ;
 
-        System.out.println(coche1.getMarca());
-        System.out.println(coche1.getMotor());
-        System.out.println(coche1.getMarca());
-        System.out.println(coche1.getPotencia());
-        System.out.println(coche1.getPuertas());
-
-//        Coche coche = new Coche("Ford");
-//
-//        coche.setMotor("Nafta");
-//        coche.setModelo("Focus");
-//        coche.setPotencia(170);
-//        coche.setPuertas(5);
-//
-//        System.out.println("Marca: " + coche.getMarca());
-//        System.out.println("Combustible: " + coche.getMotor());
-//        System.out.println("Modelo: " + coche.getModelo());
-//        System.out.println("Potencia: " + coche.getPotencia() + " c.v.");
-//        System.out.println(coche.getPuertas() + " puertas");
+        System.out.println("Marca: " + coche.getMarca());
+        System.out.println("Combustible: " + coche.getMotor());
+        System.out.println("Modelo: " + coche.getModelo());
+        System.out.println("Potencia: " + coche.getPotencia() + " c.v.");
+        System.out.println("Puertas: " + coche.getPuertas());
     }
 }
