00f4c2a07d1d283730d28f0cb3ea637881e50555
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-06-05 10:18:45 -0300

    Observer Pattern, fix nested classes.
    Curso de Java Básico

--- Java_Basico/tema11/ObserverPattern/src/Main.java
+++ Java_Basico/tema11/ObserverPattern/src/Main.java
@@ -10,31 +10,31 @@ public class Main {
 
         emisor.saluda();
     }
+}
 
-    private interface Mensajero {
-        void hanSaludado();
+interface Mensajero {
+    void hanSaludado();
+}
+
+class Emisor {
+    private final ArrayList<Mensajero> receptores = new ArrayList<>();
+
+    public void meteReceptor(Mensajero receptor) {
+        receptores.add(receptor);
     }
 
-    private static class Emisor {
-        private final ArrayList<Mensajero> receptores = new ArrayList<>();
+    public void saluda() {
+        System.out.println("¡Hola desde Emisor!");
 
-        public void meteReceptor(Mensajero receptor) {
-            receptores.add(receptor);
-        }
-
-        public void saluda() {
-            System.out.println("¡Hola desde Emisor!");
-
-            for (Mensajero receptor : receptores) {
-                receptor.hanSaludado();
-            }
+        for (Mensajero receptor : receptores) {
+            receptor.hanSaludado();
         }
     }
+}
 
-    private static class Receptor implements Mensajero {
-        @Override
-        public void hanSaludado() {
-            System.out.println("Hola en clase receptor " + this);
-        }
+class Receptor implements Mensajero {
+    @Override
+    public void hanSaludado() {
+        System.out.println("Hola en clase receptor " + this);
     }
 }
