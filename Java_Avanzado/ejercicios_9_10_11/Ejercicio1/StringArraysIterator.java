5661b8230968cf3b40f32632812fe9cf63b33219
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-06-27 16:40:03 -0300

    Ejercicio de los temas 9, 10 y 11.
    Curso de Java Avanzado

---
+++ Java_Avanzado/ejercicios_9_10_11/Ejercicio1/StringArraysIterator.java
@@ -0,0 +1,10 @@
+public class StringArraysIterator {
+    public static void main(String[] args) {
+
+        String[] words = {"pablito", "clavó", "un", "clavito"};
+
+        for (String word: words)
+            System.out.println(word);
+
+    }
+}
