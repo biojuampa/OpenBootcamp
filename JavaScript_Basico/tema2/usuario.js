61be428bd408ef64aa2c398c32f003d775915302
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-10 14:03:38 -0300

    Ejercicio tema 2
    Curso de JavaScript Básico

--- JavaScript_Basico/tema2/usuario.js
+++ JavaScript_Basico/tema2/usuario.js
@@ -1,15 +1,16 @@
-const name = "Juan Pablo Camussi";
-const age = 46;
-const isDev = true;
-const date = new Date();
-const book = {
-    title: "Clean Code",
-    author: "Robert C. Martin",
-    date: new Date(2008, 07),
-    isbn: "9780136083238",
-    publisher: "Pearson",
-    url: "https://www.oreilly.com/library/view/clean-code-a/9780136083238/"
-};
+const name = "Juan Pablo Camussi",
+      age = 46,
+      isDev = true,
+      date = new Date(),
+
+      book = {
+          title: "Clean Code",
+          author: "Robert C. Martin",
+          date: new Date(2008, 07),
+          isbn: "9780136083238",
+          publisher: "Pearson",
+          url: "https://www.oreilly.com/library/view/clean-code-a/9780136083238/"
+      };
 
 const userData = [name, age, isDev, date, book];
 
