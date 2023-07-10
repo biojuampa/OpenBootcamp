effa52810807fafb385d9e1d7eb71c6d8494b2ea
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-10 13:43:29 -0300

    Ejercicio tema 2
    Curso de JavaScript Básico

---
+++ JavaScript_Basico/tema2/Readme.md
@@ -0,0 +1,14 @@
+# Ejercicio tema 2 - JavaScript Básico
+
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
---
+++ JavaScript_Basico/tema2/usuario.js
@@ -0,0 +1,17 @@
+const name = "Juan Pablo Camussi";
+const age = 46;
+const isDev = true;
+const date = new Date();
+const book = {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    date: new Date(2008, 07),
+    isbn: "9780136083238",
+    publisher: "Pearson",
+    url: "https://www.oreilly.com/library/view/clean-code-a/9780136083238/"
+};
+
+const userData = [name, age, isDev, date, book];
+
+console.log(userData);
+
