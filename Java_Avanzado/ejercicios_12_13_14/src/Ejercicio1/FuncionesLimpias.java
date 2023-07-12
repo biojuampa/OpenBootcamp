c439ae3388e84d13aa7928d25e8548b93426a0fe
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-12 17:18:09 -0300

    Ejercicios de los temas 12, 13, 14.
    Curso de Java Avanzado

--- Java_Avanzado/ejercicios_12_13_14/src/Ejercicio1/FuncionesLimpias.java
+++ Java_Avanzado/ejercicios_12_13_14/src/Ejercicio1/FuncionesLimpias.java
@@ -1,12 +1,19 @@
 package Ejercicio1;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class FuncionesLimpias {
     public static void main(String[] args) {
-        func1();
-        func2();
+        System.out.println(calcularFactorial(5));
+        System.out.println(sumarEnteros(21, 14, 68, -81, 0, 33, -65, 128));
     }
 
-    private static void func1() {}
+    private static long calcularFactorial(int n) {
+        return IntStream.rangeClosed(1, n).reduce(1, (a, b) -> a * b);
+    }
 
-    private static String func2() {return null;}
+    private static long sumarEnteros(int... numeros) {
+        return Arrays.stream(numeros).reduce(0, Integer::sum);
+    }
 }
