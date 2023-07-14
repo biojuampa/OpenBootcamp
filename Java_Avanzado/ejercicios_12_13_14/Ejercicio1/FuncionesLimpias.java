719e556cb57dc9d6bfe10584aaeab1b0b0037441
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-14 04:49:33 -0300

    Ejercicios de los temas 12, 13, 14.
    Curso de Java Avanzado

--- Java_Avanzado/ejercicios_12_13_14/Ejercicio1/FuncionesLimpias.java
+++ Java_Avanzado/ejercicios_12_13_14/Ejercicio1/FuncionesLimpias.java
@@ -1,12 +1,16 @@
-package Ejercicio1;
-
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/*
+ * Soy consciente de que las dos funciones que he creado fallan en una regla, son demasiado simples y pueden
+ * ser reemplazadas con un inlining en el Main, pero por cuestiones prácticas y de tiempo (y falta de creatividad) he
+ * decidido que es suficiente como ejercicio de funciones limpias. Pasando por alto ese detalle, creo que por lo demás
+ * cumplo con el cometido de crear un código limpio.
+ */
 public class FuncionesLimpias {
     public static void main(String[] args) {
-        System.out.println(calcularFactorial(5));
-        System.out.println(sumarEnteros(21, 14, 68, -81, 0, 33, -65, 128));
+        System.out.println("Factorial: " + calcularFactorial(5));
+        System.out.println("Sumatoria: " + sumarEnteros(21, 14, 68, -81, 0, 33, -65, 128));
     }
 
     private static long calcularFactorial(int n) {
