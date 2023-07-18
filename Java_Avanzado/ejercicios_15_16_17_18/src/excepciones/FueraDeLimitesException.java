a3885dc823940de40e9f8a2bffc8dff8b9f43eeb
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-18 00:46:44 -0300

    (Fix) Ejercicio de los temas 15, 16, 17 y 18.
    Curso de Java Avanzado

--- Java_Avanzado/ejercicios_15_16_17_18/src/excepciones/FueraDeLimitesException.java
+++ Java_Avanzado/ejercicios_15_16_17_18/src/excepciones/FueraDeLimitesException.java
@@ -4,8 +4,4 @@ public class FueraDeLimitesException extends ArrayIndexOutOfBoundsException {
     public FueraDeLimitesException(String message) {
         super("Índice fuera de los límites: " + message);
     }
-
-    public FueraDeLimitesException(int index) {
-        super(index);
-    }
 }
