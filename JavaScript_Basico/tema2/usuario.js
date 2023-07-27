7516531f8b1d12f5a07e1c2ef5a97922897bd1ca
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-07-27 00:32:24 -0300

    Ejercicio de tema 6
    Curso de JavaScript Básico

--- JavaScript_Basico/tema2/usuario.js
+++ JavaScript_Basico/tema2/usuario.js
@@ -6,7 +6,7 @@ const userName = "Juan Pablo Camussi",
       book = {
           title: "Clean Code",
           author: "Robert C. Martin",
-          date: new Date(2008, 07),
+          date: new Date(2008, 7),
           isbn: "9780136083238",
           publisher: "Pearson",
           url: "https://www.oreilly.com/library/view/clean-code-a/9780136083238/"
